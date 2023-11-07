5679981e2b3f2457e2e3979b33a6fe76934afa13
Divyanshu766 <lalitarawat90123@gmail.com>
2023-11-08 02:50:06 +0530

    Adding Skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -21,6 +21,27 @@ const Skills = () => {
       Image: "https://img.icons8.com/quill/250/java-coffee-cup-logo.png",
       name: "Java",
     },
+    {
+      Image: "https://img.icons8.com/windows/250/node-js.png",
+      name: "Node JS",
+    },
+    {
+      Image: "https://img.icons8.com/ios/250/database-options.png",
+      name: "Mongo DB",
+    },
+    {
+      Image: "https://img.icons8.com/ios/250/express-js.png",
+      name: "Express JS",
+    },
+    {
+      Image:
+        "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/250/external-redux-an-open-source-javascript-library-for-managing-application-state-logo-bold-tal-revivo.png",
+      name: "Redux",
+    },
+    {
+      Image: "https://img.icons8.com/carbon-copy/250/python.png",
+      name: "Python",
+    },
   ]);
   return (
     <>
