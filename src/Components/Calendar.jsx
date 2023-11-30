d873198cae159d7a4df9e676ef989769d2f50cc1
Divyanshu766 <lalitarawat90123@gmail.com>
2023-11-30 17:54:18 +0530

    Add responsiveness

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import GitHubCalendar from "react-github-calendar";
 
 const Calendar = () => {
+  const [blockSize, setBlockSize] = useState(window.innerWidth < 700 ? 8 : 20);
+  const selectLastHalfYear = (contributions) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 6;
+    console.log(window.innerWidth);
+    return contributions.filter((activity) => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBlockSize(window.innerWidth < 700 ? 8 : 20);
+    };
+
+    // Attach the event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <section id="calendar" className="react-activity-calendar">
       <div>
@@ -8,9 +40,11 @@ const Calendar = () => {
       </div>
       <div id="gitCalendar">
         <GitHubCalendar
-          colorScheme={"dark"}
-          blockSize={17}
+          colorScheme={"light"}
+          blockSize={blockSize}
           username="divyanshu766"
+          transformData={selectLastHalfYear}
+          blockRadius="2"
         />
       </div>
     </section>
